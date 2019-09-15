01bb5573ef1974bead7506e9e0265121852e48e8
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2019-09-15 13:32:31 +0200

    Primeira creación

---
+++ js/config.js
@@ -0,0 +1,25 @@
+// More info about config & dependencies:
+// - https://github.com/hakimel/reveal.js#configuration
+// - https://github.com/hakimel/reveal.js#dependencies
+Reveal.initialize({
+  history: true,
+  transition: 'linear',
+
+  math: {
+    // mathjax: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js',
+    config: 'TeX-AMS_HTML-full',
+    TeX: {
+      Macros: {
+        R: '\\mathbb{R}',
+        set: [ '\\left\\{#1 \\; ; \\; #2\\right\\}', 2 ]
+      }
+    }
+  },
+  dependencies: [
+    { src: '../plugin/markdown/marked.js' },
+    { src: '../plugin/markdown/markdown.js' },
+    { src: '../plugin/notes/notes.js', async: true },
+    { src: '../plugin/highlight/highlight.js', async: true },
+    { src: '../plugin/math/math.js', async: true }
+  ]
+});
