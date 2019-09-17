eed9c8b67a24c7fbb7558f9ee7eae4abc4e4fec5
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2019-09-17 21:55:07 +0200

    Engadidas presentacions día 2

--- js/config.js
+++ js/config.js
@@ -15,11 +15,24 @@ Reveal.initialize({
       }
     }
   },
+
+  chalkboard: {
+		theme: "whiteboard",
+	},
+
   dependencies: [
     { src: '../plugin/markdown/marked.js' },
     { src: '../plugin/markdown/markdown.js' },
     { src: '../plugin/notes/notes.js', async: true },
     { src: '../plugin/highlight/highlight.js', async: true },
-    { src: '../plugin/math/math.js', async: true }
-  ]
+    { src: '../plugin/math/math.js', async: true },
+    { src: '../plugin/chalkboard/chalkboard.js' },
+  ],
+  
+  keyboard: {
+	    66: function() { RevealChalkboard.toggleChalkboard() },	// toggle chalkboard when 'b' is pressed
+	    46: function() { RevealChalkboard.clear() },	// clear chalkboard when 'DEL' is pressed
+	     8: function() { RevealChalkboard.reset() },	// reset chalkboard data on current slide when 'BACKSPACE' is pressed
+	    68: function() { RevealChalkboard.download() },	// downlad recorded chalkboard drawing when 'd' is pressed
+	},
 });
