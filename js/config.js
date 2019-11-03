192fa6e13746f23799f9b1dcb3ff47e8b28e3f1d
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2019-11-03 22:47:18 +0100

    Engadidas novas presentacións

--- js/config.js
+++ js/config.js
@@ -32,12 +32,13 @@ Reveal.initialize({
 
 
   keyboard: {
-	    66: function() { RevealChalkboard.toggleChalkboard() },	// toggle chalkboard when 'b' is pressed
-	    46: function() { RevealChalkboard.clear() },	// clear chalkboard when 'DEL' is pressed
-	     8: function() { RevealChalkboard.reset() },	// reset chalkboard data on current slide when 'BACKSPACE' is pressed
-	    68: function() { RevealChalkboard.download() },	// downlad recorded chalkboard drawing when 'd' is pressed
-      83: function() {Reveal.soluciones.cambiar()}
-
+	    66: function()   { RevealChalkboard.toggleChalkboard() },	// toggle chalkboard when 'b' is pressed
+	    46: function()   { RevealChalkboard.clear() },	// clear chalkboard when 'DEL' is pressed
+	     8: function()   { RevealChalkboard.reset() },	// reset chalkboard data on current slide when 'BACKSPACE' is pressed
+	    68: function()   { RevealChalkboard.download() },	// downlad recorded chalkboard drawing when 'd' is pressed
+      83: function()   { Reveal.soluciones.cambiar()}, // amosar solucións
+      171: function () { Reveal.fontSize.cambiar(1)}, // aumentar tamaño letra
+      173: function () { Reveal.fontSize.cambiar(-1)} // diminuir tamaño letra
 	},
 });
 
@@ -49,5 +50,17 @@ Reveal.soluciones = {
 
     if (this.activado) div.classList.add("mostrar-solucion");
     else div.classList.remove("mostrar-solucion");
+  }
+}
+
+Reveal.fontSize = {
+  current: 10,
+  cambiar (cantidade) {
+    this.current += cantidade;
+
+    var problemas = document.getElementsByClassName("problema");
+    for (var i = 0; i < problemas.length; i++) {
+      problemas[i].style.fontSize = String (this.current/10) + "em";
+    }
   }
 }
