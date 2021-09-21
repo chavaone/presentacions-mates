768d48020c85238f07c005a2349ae831891ca25e
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2021-09-21 20:28:44 +0200

    Incluír scraper a tex

--- utils/problems2latex/problems_scraper.py
+++ utils/problems2latex/problems_scraper.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python
 
-
 import sys
 from bs4 import BeautifulSoup
-
 
 def help():
     pass
 
+
+def apartado2latex (apartado):
+    try:
+        solucion = apartado.find("span", class_="solucion").extract()
+    except:
+        solucion = ""
+    texto = apartado.getText().strip()
+    texto = texto.replace("\[", "$").replace("\]", "$")
+    return "\item " + texto
+
 def html2latex (problem):
-    
+    enunciado_tag = problem.find("p")
+    enunciado_tag.find("span", class_="numero").extract()
+    enunciado_txt = enunciado_tag.getText().replace("\[", "$").replace("\]", "$")
+    try:
+        apartados = [apartado2latex(apartado) for apartado in problem.find("ol", class_="problema__apartados").find_all("li")]
+    except:
+        apartados = []
+
+    return "\\Exercicio " + enunciado_txt + "\n\\begin{enumerate}[topsep=0pt]\n " + "\n".join(apartados) + "\n\\end{enumerate}\n\n"
+
 
 def main():
 
     if len(sys.argv) != 2:
         help()
+        sys.exit()
 
     htmlTxt = ""
 
 
-    with open(argv[1]) as file:
+    with open(sys.argv[1]) as file:
         htmlTxt = file.read()
 
     soup = BeautifulSoup(htmlTxt, 'html.parser')
