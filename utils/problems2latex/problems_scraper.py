23c9406e4eb0c5ea7ac62272edc5e2c53f22f8fb
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2021-09-26 11:34:27 +0200

    Mellorado Scraper

--- utils/problems2latex/problems_scraper.py
+++ utils/problems2latex/problems_scraper.py
@@ -6,26 +6,60 @@ from bs4 import BeautifulSoup
 def help():
     pass
 
+def tabla2latex (tabla):
+    return ""
 
 def apartado2latex (apartado):
     try:
         solucion = apartado.find("span", class_="solucion").extract()
     except:
         solucion = ""
+    try:
+        numero = apartado.find("span", class_="numero").extract()
+    except:
+        pass
     texto = apartado.getText().strip()
     texto = texto.replace("\[", "$").replace("\]", "$")
     return "\item " + texto
 
 def html2latex (problem):
-    enunciado_tag = problem.find("p")
-    enunciado_tag.find("span", class_="numero").extract()
-    enunciado_txt = enunciado_tag.getText().replace("\[", "$").replace("\]", "$")
     try:
-        apartados = [apartado2latex(apartado) for apartado in problem.find("ol", class_="problema__apartados").find_all("li")]
+        enunciado_tag = problem.find("p")
+        enunciado_tag.find("span", class_="numero").extract()
+        enunciado_txt = enunciado_tag.getText().replace("\[", "$").replace("\]", "$")
+        enunciado_txt = enunciado_txt.replace("%", "\%")
+    except:
+        enunciado_txt = ""
+
+    try:
+        tabla = problem.find("table").extract()
+        tabla_txt = tabla2latex(tabla)
+    except:
+        tabla_txt = ""
+
+    try:
+        apartados = [apartado2latex(apartado) for apartado in problem.find(class_="problema__apartados").find_all("li")]
     except:
         apartados = []
 
-    return "\\Exercicio " + enunciado_txt + "\n\\begin{enumerate}[topsep=0pt]\n " + "\n".join(apartados) + "\n\\end{enumerate}\n\n"
+    try:
+        quote = problema.find("q").extract()
+        quote_txt = quote.getText()
+    except:
+        quote_txt = ""
+
+    ret = "\\Exercicio " + enunciado_txt + "\n"
+
+    if quote_txt:
+        ret += "\\begin{quote}\n" + quote_txt + "\n\\end{quote}\n"
+
+    if tabla_txt:
+        ret += tabla_txt + "\n"
+
+    if apartados:
+        ret += "\n\\begin{enumerate}[topsep=0pt]\n" + "\n\t".join(apartados) + "\n\\end{enumerate}\n\n"
+
+    return ret
 
 
 def main():
