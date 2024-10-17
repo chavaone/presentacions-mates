cd46047a7fd009e9a7f396e73fff74bdf5b30642
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2024-10-17 20:14:01 +0200

    Engadir de novo funcións matemáticas

--- js/config.esm.js
+++ js/config.esm.js
@@ -28,6 +28,6 @@ Reveal.initialize({
     }
   },
   // ...
-  plugins: [ RevealChalkboard, RevealCustomControls ],
+  plugins: [ RevealChalkboard, RevealCustomControls, RevealMath.KaTeX ],
   // ...
 });
