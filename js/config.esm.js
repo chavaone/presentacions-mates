4534a62ebd06b55e5190d26b6b81e377356d6fc1
Marcos Chavarría Teijeiro <chavarria1991@gmail.com>
2024-10-28 22:46:08 +0100

    Engadida fórmulas matemáticas

--- js/config.esm.js
+++ js/config.esm.js
@@ -1,33 +1,46 @@
-import Reveal from '/dist/reveal.esm.js';
+import Reveal from "/dist/reveal.esm.js";
 
 Reveal.initialize({
   customcontrols: {
     controls: [
-      { icon: '<i class="fa fa-pen-square"></i>',
-        title: 'Toggle chalkboard (B)',
-        action: 'RevealChalkboard.toggleChalkboard();'
+      {
+        icon: '<i class="fa fa-pen-square"></i>',
+        title: "Toggle chalkboard (B)",
+        action: "RevealChalkboard.toggleChalkboard();",
       },
-      { icon: '<i class="fa fa-pen"></i>',
-        title: 'Toggle notes canvas (C)',
-        action: 'RevealChalkboard.toggleNotesCanvas();'
+      {
+        icon: '<i class="fa fa-pen"></i>',
+        title: "Toggle notes canvas (C)",
+        action: "RevealChalkboard.toggleNotesCanvas();",
       },
-      { icon: '<i class="fa fa-paragraph"></i><span>+</span>',
+      {
+        icon: '<i class="fa fa-paragraph"></i><span>+</span>',
         title: "Subir letra",
-        action: 'revealPlugins.fontSize.cambiar(1);'
+        action: "revealPlugins.fontSize.cambiar(1);",
       },
-      { icon: '<i class="fa fa-paragraph"></i><span>-</span>',
+      {
+        icon: '<i class="fa fa-paragraph"></i><span>-</span>',
         title: "Subir letra",
-        action: 'revealPlugins.fontSize.cambiar(-1);'
-      }
-
-    ]
+        action: "revealPlugins.fontSize.cambiar(-1);",
+      },
+    ],
   },
   chalkboard: {
     eraser: {
-      radius: 100
-    }
+      radius: 100,
+    },
+  },
+  katex: {
+    version: "latest",
+    delimiters: [
+      { left: "$$", right: "$$", display: true },
+      { left: "$", right: "$", display: false },
+      { left: "\\(", right: "\\)", display: true },
+      { left: "\\[", right: "\\]", display: true },
+    ],
+    ignoredTags: ["script", "noscript", "style", "textarea", "pre"],
   },
   // ...
-  plugins: [ RevealChalkboard, RevealCustomControls, RevealMath.KaTeX ],
+  plugins: [RevealChalkboard, RevealCustomControls, RevealMath.KaTeX],
   // ...
 });
